f5b9bae5c9c759ffa581ce89c0da55011ae4aba3
Filippo Rossi <filipporossi@hey.com>
2023-02-08 00:11:52 +0100

    feat: backgrounds

--- player.js
+++ player.js
@@ -17,6 +17,8 @@ window.onload = () =>
     head.appendChild(style);
 
     const card = document.querySelector(".card");
+    card.style.opacity = 0.97;
+
     const cardBody = document.querySelector(".card-body");
     const nowplaying = document.querySelector(".nowplaying");
 
@@ -67,11 +69,7 @@ window.onload = () =>
     </a>`;
     card.appendChild(footerLinks);
 
-    const footer = document.querySelector("#footer");
-    footer.innerHTML = "Radio Poggiolo, la radio dei veri sborati.";
-
-    let oldSong = "";
-
+    let previousImgSong = "";
     async function update() {
       const data = await fetch("/api/frontend/dashboard/stations", {
         headers: {
@@ -83,11 +81,62 @@ window.onload = () =>
         (element) => element.station.public_player_url == window.location.href
       );
       listeners.innerText = `Sborati in ascolto: ${radio.listeners.total}`;
-      if (radio.playing_next.song.text != oldSong) {
+      if (radio.playing_next.song.text != previousImgSong) {
         next.innerText = (radio.playing_next.song.text + " // ").repeat(4);
-        oldSong = radio.playing_next.song.text;
+        previousImgSong = radio.playing_next.song.text;
       }
       setTimeout(update, 5000);
     }
     update();
+
+    async function backgrounds() {
+      const baseUrl = "https://poggiolo.github.io/radio/backgrounds";
+      const imageUrls = await fetch(`${baseUrl}/index.txt`)
+        .then((res) => res.text())
+        .then((index) => index.trim().split("\n"));
+
+      const body = document.querySelector("body");
+
+      const footer = document.querySelector("#footer");
+      footer.innerHTML = "Radio Poggiolo, la radio dei veri sborati.";
+
+      const images = [];
+
+      for (const imageUrl of imageUrls) {
+        const author = imageUrl.split("/")[0];
+        const img = document.createElement("img");
+        img.src = `${baseUrl}/${imageUrl}`;
+        img.style.position = "absolute";
+        img.style.top = 0;
+        img.style.left = 0;
+        img.style.width = "100%";
+        img.style.height = "100%";
+        img.style.objectFit = "cover";
+        img.style.zIndex = -1;
+        img.style.transition = "opacity 0.2s";
+        img.style.opacity = 0;
+        body.appendChild(img);
+        images.push([img, author]);
+      }
+
+      const interval = 15 * 60 * 1000;
+
+      let previousImg = null;
+      function updateBackground() {
+        const index = Math.round(Date.now() / interval) % images.length;
+        const [img, author] = images[index];
+        img.style.opacity = 1;
+        if (previousImg != null) {
+          previousImg.style.opacity = 0;
+        }
+        previousImg = img;
+
+        footer.innerHTML = `Radio Poggiolo, la radio dei veri sborati.<br>Sfondo by ${author}`;
+        setTimeout(updateBackground, interval);
+      }
+
+      updateBackground();
+    }
+
+    backgrounds();
   }, 10);
